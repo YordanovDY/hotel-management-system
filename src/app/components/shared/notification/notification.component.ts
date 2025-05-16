fe7b764853d4794866340d1017826c673414b8cd
YordanovDY <yordanovdy23@gmail.com>
2025-05-16 15:22:29 +0300

    Clear notification after 6 seconds

--- src/app/components/shared/notification/notification.component.ts
+++ src/app/components/shared/notification/notification.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NotificationAlert } from './notification.types';
+import { interval, takeWhile, tap } from 'rxjs';
 
 @Component({
   selector: 'app-notification',
@@ -11,7 +12,19 @@ import { NotificationAlert } from './notification.types';
 export class NotificationComponent {
   messages: NotificationAlert[] = [];
 
-  showNotification(notification: NotificationAlert){
+  showNotification(notification: NotificationAlert) {
     this.messages.push(notification);
+
+    if (this.messages.length === 1) {
+      this.removeOldNotifications();
+    }
+  }
+
+  private removeOldNotifications() {
+    interval(6000).pipe(
+      takeWhile(() => this.messages.length > 0),
+      tap(() => this.messages.shift())
+    )
+      .subscribe();
   }
 }
